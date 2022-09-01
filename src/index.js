52c2a19d1bfc89611211d0063403a64e17f0adbb
Yushkov88 <yushkov1988@gmail.com>
2022-09-01 20:02:01 +0300

    part 1 hw

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const searchInput = document.getElementById('search-box');
+
+searchInput.addEventListener(
+  'input',
+  debounce(event => {
+    fetchCountries(event.target.value.trim()).then(data => {
+      console.log(data);
+    });
+  }, DEBOUNCE_DELAY)
+);
